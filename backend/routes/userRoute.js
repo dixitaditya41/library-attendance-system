4e9e3786b42d51b424d24097c59659a6a8bed673
Aditya dixit <85583554+dixitaditya41@users.noreply.github.com>
2024-11-16 11:51:50 +0530

    some changes by praveer

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 
 
-router.post('/register', registerRoute);
-router.post('/login', loginRoute);
+router.post('register', registerRoute);
+router.post('login', loginRoute);
 router.get('/logout',authenticate,(req,res)=>{
     res.clearCookie('jwt');
     res.status(200).json({message:"Logout Successful"});
