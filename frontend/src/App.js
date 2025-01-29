4a84da4c49dfb982b75de872fac351e8c22ce0fd
adityawikiance <adityadixit@wikiance.com>
2025-01-30 03:30:52 +0530

    logout added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Signup from './components/SignUp';
 import Profile from "./Pages/Profile";
 import Login from './components/Login';
+import Logout from "./Pages/LogOut";
 import Entry from './Pages/Entry';
 import Exit from './Pages/Exit';
 
@@ -20,6 +21,7 @@ function App() {
         <Route path="/" element={<Home isLoggedin ={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
         <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn} />} />
         <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path ="/logout" element={<Logout setIsLoggedIn={setIsLoggedIn} />} />
         <Route path="/entry" element={<Entry setIsLoggedIn={setIsLoggedIn} />} />
         <Route path="/exit" element={<Exit setIsLoggedIn={setIsLoggedIn} />} />
         <Route path="/profile" element={<Profile setIsLoggedIn={setIsLoggedIn} />} />
